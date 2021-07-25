069adc11df8d9b9fc8e7ec9ee558baa4f5454af0
stvvvvvv <stv5458@gmail.com>
2021-07-25 22:04:27 +0300

    Fix
    1. Deleted vue-router

--- src/main.js
+++ src/main.js
@@ -3,6 +3,5 @@ import '@/assets/scss/styles.scss'
 import { createApp } from 'vue'
 import App from './App.vue'
 import store from './store'
-import router from './router'
 
-createApp(App).use(router).use(store).mount('#app')
+createApp(App).use(store).mount('#app')
